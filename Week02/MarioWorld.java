7501f8d2d4f3e5a602b9d92816014169e977702f
Derek Peacock <derek.peacock@yahoo.com>
2022-04-28 19:12:49 +0100

    renamed the world class

--- Week02/MarioWorld.java
+++ Week02/MarioWorld.java
@@ -6,14 +6,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version 1.0
  */
-public class MyWorld extends World
+public class MarioWorld extends World
 {
 
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
+    public MarioWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(12, 10, 30); 
