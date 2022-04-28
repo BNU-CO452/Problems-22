520936419535173fb0b5a4f13e437fe68b681c0c
Derek Peacock <derek.peacock@yahoo.com>
2022-04-28 19:10:17 +0100

    Initial commit

---
+++ Week02/MyWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A new version of the Mario Game
+ * 
+ * @author (your name) 
+ * @version 1.0
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(12, 10, 30); 
+    }
+}
