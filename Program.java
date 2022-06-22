70c9087055d189d5bb8720c90e9ba5efe64d6b41
Derek Peacock <derek.peacock@yahoo.com>
2022-06-22 14:38:59 +0100

    Added helper classes

--- Program.java
+++ Program.java
@@ -3,14 +3,20 @@ public class Program
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
     public static void main(String[] args)
     {
+        System.out.println(ConsoleColours.ANSI_BG_GREEN);77
+        System.out.println(ConsoleColours.ANSI_BRIGHT_YELLOW);
         System.out.println(CONSOLE_CLEAR);
+
         System.out.println("CO452 Programming Concepts 2022/23");
         System.out.println("==================================");
         System.out.println();
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
 
-        Week01 exercise01 = new Week01();
-        exercise01.run();
+        InputReader reader = new InputReader();
+        int age = reader.getInt("Please enter your age > ");
+
+        //Week01 exercise01 = new Week01();
+        //exercise01.run();
     }
 }
